2b329a74acefb940f6b5b188864a3ec3dacd8a5d
LSchultz114 <lschult2@gmu.edu>
2017-06-21 09:52:16 -0400

    Update run_test.sh
    Since eqpy.py is now included in the folder, the addition of the ext/EQ-py/ to the pythonpath is no longer necessary

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 SPEAR_ROOT=$( cd $( dirname $0 )/.. ; /bin/pwd )
-export PYTHONPATH=$SPEAR_ROOT/python:$SPEAR_ROOT/ext/EQ-Py
+export PYTHONPATH=$SPEAR_ROOT/python
 python $SPEAR_ROOT/test/test.py
